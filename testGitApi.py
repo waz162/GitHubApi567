0e8769cc21a717c5586097e9b4c3e2748a8da9e4
waz162 <43581160+waz162@users.noreply.github.com>
2021-03-16 17:04:38 -0400

    Added mock to unit testing

--- testGitApi.py
+++ testGitApi.py
@@ -1,14 +1,26 @@
 import unittest
 from GitHubApi567 import pull_info
+from unittest import mock
+
 
 class testGitApi(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
-
     def testPullTrue(self):
-        self.assertEqual(pull_info('waz162'), "Successful", "The pull was succesful")
+        with mock.patch('GitHubApi567.pull_info', return_value=b'Successful\nPlease enter a valid username\n'):
+            actual_result = pull_info('waz162')
+
+        expected_result = 'Successful'
+        self.assertIn(expected_result, actual_result)
+        
 
     def testPullFalse(self):
-        self.assertEqual(pull_info('fdjskfdsakf'), "Please enter a valid username", "The pull was failed, the username doesn't work")
+        with mock.patch('GitHubApi567.pull_info', return_value=b'Successful\nPlease enter a valid username\n'):
+            actual_result = pull_info('fdjskfdsakf')
+        
+        expected_result = 'Please enter a valid username'
+        self.assertIn(expected_result, actual_result)
+    
+
 
 if __name__ == '__main__':
     print('Running unit tests')
